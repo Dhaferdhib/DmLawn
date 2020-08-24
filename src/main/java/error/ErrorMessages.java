66145f210b7fbb6d9fdac7e819d0dbd34d19e91b
Dhafer <dhafer_dhib@carrefour.com>
2020-08-24 14:27:19 +0200

    small changes to new branch

--- src/main/java/error/ErrorMessages.java
+++ src/main/java/error/ErrorMessages.java
@@ -5,6 +5,6 @@ public class ErrorMessages {
     public static final String NO_ACTIONS_FOUND = "No Actions found for this mower";
     public static final String ACTION_ENUM_NOT_FOUND = "Action entered is not covered by the application";
     public static final String ORIENTATION_ENUM_NOT_FOUND = "Orientation entered is not covered by the application";
-    public static final String NEGATIVE_DIMENSIONS = "Dimensions can't be negative";
+    public static final String NEGATIVE_DIMENSIONS = "Dimensions can not be negative";
 
 }
